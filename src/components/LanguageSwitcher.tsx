3e2efc011426dc188a851328fc0581d3930d2b81
Kirill Anikin <anikinkirill22@gmail.com>
2022-03-31 01:55:27 +0300

    feat: locales, styles of Dashboard changed

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,68 @@
+import { CSSProperties, useMemo, useCallback } from "react";
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Divider,
+} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+import { GrLanguage } from "react-icons/gr";
+import { ReactComponent as FlagRu } from "../../node_modules/flag-icons/flags/4x3/ru.svg";
+import { ReactComponent as FlagEn } from "../../node_modules/flag-icons/flags/4x3/gb.svg";
+
+interface Props {
+  style: CSSProperties;
+}
+
+const shortToFullForm: Record<string, string> = {
+  ru: "Russian",
+  en: "English",
+};
+
+const LanguageSwitcher = ({ style }: Props) => {
+  const { t, i18n } = useTranslation();
+  //   const currentLanguage = useMemo(() => {
+  //     return shortToFullForm[i18n.language];
+  //   }, [i18n.language]);
+
+  const lang = i18n.language;
+
+  const setLang = useCallback(
+    (lang: string) => {
+      i18n.changeLanguage(lang);
+    },
+    [i18n]
+  );
+
+  return (
+    <Box style={style}>
+      <Menu>
+        <MenuButton
+          display="flex"
+          alignItems="center"
+          as={Button}
+          leftIcon={<GrLanguage />}
+        >
+          {t(lang)}
+        </MenuButton>
+        <MenuList>
+          <MenuItem onClick={() => setLang("en")}>
+            <FlagEn style={{ maxHeight: 18, marginRight: 10 }} />
+            {t("en")}
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={() => setLang("ru")}>
+            <FlagRu style={{ maxHeight: 18, marginRight: 10 }} />
+            {t("ru")}
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+
+export default LanguageSwitcher;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,16 +9,60 @@ import {
   Icon,
   Text,
   Tooltip,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Flex,
+  Stack,
 } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 
+import { Project } from "../models/Project";
 import { fetchProjects, projectsSelector } from "../store/slices/projectsSlice";
 import { LoadingStatus } from "../types/common";
-import { Project } from "../models/Project";
+import LanguageSwitcher from "../components/LanguageSwitcher";
 
 import { BsClock, BsListTask } from "react-icons/bs";
 import { BiTask, BiDollar } from "react-icons/bi";
+
+/*
+ * TODO: Needs to be moved out of here
+ */
+interface ProjectModalProps {
+  type: "new" | "edit";
+  isOpen: boolean;
+  onClose: () => void;
+  data?: Partial<Project>;
+}
+const ProjectModal = ({ isOpen, onClose, type, data }: ProjectModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody></ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 /*
  * TODO: Needs to be moved out of here
@@ -29,109 +73,109 @@ interface ProjectsListProps {
 
 const ProjectsList = ({ list }: ProjectsListProps) => {
   const { t } = useTranslation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <SimpleGrid minChildWidth="250px" spacing="10px">
-      {list.map(
-        ({
-          name,
-          timeSpent = 90,
-          tasksCompleted = 10,
-          tasksTotal = 15,
-          moneyEarned = 555,
-        }) => (
-          <Box
-            height={150}
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
-            // _hover={{
-            //   cursor: "pointer",
-            //   background: "gray.100",
-            //   color: "teal.500",
-            // }}
-          >
-            <Box p={6}>
-              <Heading fontSize="m">{name}</Heading>
-            </Box>
+    <>
+      <SimpleGrid minChildWidth="250px" spacing="10px">
+        {list.map(
+          ({
+            name,
+            timeSpent = 90,
+            tasksCompleted = 10,
+            tasksTotal = 15,
+            moneyEarned = 555,
+          }) => (
             <Box
-              borderTopWidth="1px"
-              height="40px"
+              height={150}
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
               display="flex"
-              justifyContent="space-evenly"
+              flexDirection="column"
+              justifyContent="space-between"
             >
-              <Tooltip label={t("dashboard.TIME_SPENT")} fontSize="md">
-                <Box
-                  cursor="help"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Icon as={BsClock} />
-                  <Text ml={2}>{timeSpent}</Text>
-                </Box>
-              </Tooltip>
-              <Divider orientation="vertical" />
-              <Tooltip label={t("dashboard.TASKS_TOTAL")} fontSize="md">
-                <Box
-                  cursor="help"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Icon as={BsListTask} />
-                  <Text ml={2}>{tasksTotal}</Text>
-                </Box>
-              </Tooltip>
-              <Divider orientation="vertical" />
-              <Tooltip label={t("dashboard.TASKS_COMPLETED")} fontSize="md">
-                <Box
-                  cursor="help"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Icon as={BiTask} />
-                  <Text ml={2}>{tasksCompleted}</Text>
-                </Box>
-              </Tooltip>
-              <Divider orientation="vertical" />
-              <Tooltip label={t("dashboard.MONEY_EARNED")} fontSize="md">
-                <Box
-                  cursor="help"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Icon as={BiDollar} />
-                  <Text ml={2}>{moneyEarned}</Text>
-                </Box>
-              </Tooltip>
+              <Box p={6}>
+                <Heading fontSize="m">{name}</Heading>
+              </Box>
+              <Box
+                borderTopWidth="1px"
+                height="40px"
+                display="flex"
+                justifyContent="space-evenly"
+              >
+                <Tooltip label={t("dashboard.TIME_SPENT")} fontSize="md">
+                  <Box
+                    cursor="help"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Icon as={BsClock} />
+                    <Text ml={2}>{timeSpent}</Text>
+                  </Box>
+                </Tooltip>
+                <Divider orientation="vertical" />
+                <Tooltip label={t("dashboard.TASKS_TOTAL")} fontSize="md">
+                  <Box
+                    cursor="help"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Icon as={BsListTask} />
+                    <Text ml={2}>{tasksTotal}</Text>
+                  </Box>
+                </Tooltip>
+                <Divider orientation="vertical" />
+                <Tooltip label={t("dashboard.TASKS_COMPLETED")} fontSize="md">
+                  <Box
+                    cursor="help"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Icon as={BiTask} />
+                    <Text ml={2}>{tasksCompleted}</Text>
+                  </Box>
+                </Tooltip>
+                <Divider orientation="vertical" />
+                <Tooltip label={t("dashboard.MONEY_EARNED")} fontSize="md">
+                  <Box
+                    cursor="help"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Icon as={BiDollar} />
+                    <Text ml={2}>{moneyEarned}</Text>
+                  </Box>
+                </Tooltip>
+              </Box>
             </Box>
-          </Box>
-        )
-      )}
-      <Box
-        p={5}
-        minHeight={100}
-        borderWidth={2}
-        borderStyle={"dotted"}
-        borderRadius="lg"
-        overflow="hidden"
-        _hover={{
-          cursor: "pointer",
-          background: "gray.100",
-          color: "teal.500",
-        }}
-      >
-        <Center h="100%">
-          <Heading fontSize="m">{t("dashboard.ADD_NEW")}</Heading>
-        </Center>
-      </Box>
-    </SimpleGrid>
+          )
+        )}
+        <Box
+          p={5}
+          minHeight={100}
+          borderWidth={2}
+          borderStyle={"dotted"}
+          borderRadius="lg"
+          overflow="hidden"
+          _hover={{
+            cursor: "pointer",
+            background: "gray.100",
+            color: "teal.500",
+          }}
+          onClick={onOpen}
+        >
+          <Center h="100%">
+            <Heading fontSize="m">{t("dashboard.ADD_NEW")}</Heading>
+          </Center>
+        </Box>
+      </SimpleGrid>
+      <ProjectModal type="new" isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
@@ -150,22 +194,52 @@ const Dashboard = () => {
   }, [dispatch]);
 
   return (
-    <Container maxW="container.xl">
-      <Heading p={5} fontSize={"xl"}>
-        {t("dashboard.MY_PROJECTS")}
-      </Heading>
-      <Divider mb={10} />
-      <ProjectsList list={projects.list} />
-      {/* {status === LoadingStatus.Loading && <>Loading...</>}
+    <>
+      <LanguageSwitcher style={{ position: "absolute", top: 5, right: 5 }} />
+      <Box>Sections</Box>
+
+      <Container maxW="container.lg">
+        <section id="projects">
+          <Heading p={5} fontSize={"xl"}>
+            {t("dashboard.MY_PROJECTS")}
+          </Heading>
+          <Divider mb={10} />
+          <ProjectsList list={projects.list} />
+        </section>
+        {/* {status === LoadingStatus.Loading && <>Loading...</>}
       {status === LoadingStatus.Failed && <>Failed...</>}
       {status === LoadingStatus.Succeeded && <>Succeed...</>}
       {status === LoadingStatus.Idle && <>Idle...</>} */}
+        <section id="recent-tasks">
+          <Heading mt={12} p={5} fontSize={"xl"}>
+            {t("dashboard.RECENT_TASKS")}
+          </Heading>
+          <Divider mb={10} />
+          <Stack>
+            <Skeleton height="40px" />
+            <Skeleton height="40px" />
+            <Skeleton height="40px" />
+            <Skeleton height="40px" />
+            <Skeleton height="40px" />
+          </Stack>
+        </section>
 
-      <Heading mt={12} p={5} fontSize={"xl"}>
-        {t("dashboard.RECENT_TASKS")}
-      </Heading>
-      <Divider mb={10} />
-    </Container>
+        <section id="stats">
+          <Heading mt={12} p={5} fontSize={"xl"}>
+            {t("dashboard.STATS")}
+          </Heading>
+          <Divider mb={10} />
+          <Box>
+            <Skeleton mt="4" height="300px" />
+            <Flex justify="space-between">
+              <Skeleton mt="4" width="33%" height="150px" />
+              <Skeleton mt="4" width="33%" height="150px" />
+              <Skeleton mt="4" width="33%" height="150px" />
+            </Flex>
+          </Box>
+        </section>
+      </Container>
+    </>
   );
 };
 
